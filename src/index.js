0bc509950836ea6b123198c70de8b04c7380f0a5
ossaritas <81383004+ossaritas@users.noreply.github.com>
2021-09-14 22:06:54 +0300

    bug solved

--- src/index.js
+++ src/index.js
@@ -2,22 +2,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./store/auth-context";
 
-import { initializeApp } from "firebase/app";
-
 import "./index.css";
 import App from "./App";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAkIMWUOMuFlhRIJVCRGwrThBcWTCZDc_s",
-  authDomain: "react-productivity-app-56f40.firebaseapp.com",
-  databaseURL:
-    "https://react-productivity-app-56f40-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "react-productivity-app-56f40",
-  storageBucket: "react-productivity-app-56f40.appspot.com",
-  messagingSenderId: "122096437014",
-  appId: "1:122096437014:web:ea2ebb0c8de74ac9d0b237",
-};
-initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <AuthContextProvider>
